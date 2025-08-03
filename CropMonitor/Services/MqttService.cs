af35c825a80889f3525ee3aa5626258f02dd8568
Isaac Ramirez <isaacrdmc@gmail.com>
2025-08-03 15:11:21 -0600

    Encapsulamos posibles errores

--- CropMonitor/Services/MqttService.cs
+++ CropMonitor/Services/MqttService.cs
@@ -33,118 +33,171 @@ namespace CropMonitor.Services
         // Tambien guardamos los datos en la BD.
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            // OPCIÓN 1: Con MqttFactory (compatible con v5)
-            //var factory = new MqttFactory();
-            //_mqttClient = factory.CreateMqttClient();
-
-            // OPCIÓN 2: Con MqttClientFactory (nuevo en v5) - Más directo
-             var clientFactory = new MqttClientFactory();
-            _mqttClient = clientFactory.CreateMqttClient();
-
-            // OPCIÓN 3: Directo (más simple para v5)
-            // _mqttClient = new MqttClientFactory().CreateMqttClient();
-
-
-
-
-            // Nos suscribimos a los topics cuando el cliente se conecte
-            _mqttClient.ConnectedAsync += async e =>
+            try
             {
-                // 
-                Console.WriteLine("### COnectando con el broker ###");
+                // OPCIÓN 1: Con MqttFactory (compatible con v5)
+                //var factory = new MqttFactory();
+                //_mqttClient = factory.CreateMqttClient();
 
-                // Nos suscribimos a los topics de entradas:
-                await _mqttClient.SubscribeAsync("valor-humedad-1");
-                await _mqttClient.SubscribeAsync("valor-humedad-2");
-                await _mqttClient.SubscribeAsync("valor-humedad-3");
-                await _mqttClient.SubscribeAsync("valor-humedad-4");
+                // OPCIÓN 2: Con MqttClientFactory (nuevo en v5) - Más directo
+                var clientFactory = new MqttClientFactory();
+                _mqttClient = clientFactory.CreateMqttClient();
 
-                Console.WriteLine("## Subscrito a los topics ###");
-            };
+                // OPCIÓN 3: Directo (más simple para v5)
+                // _mqttClient = new MqttClientFactory().CreateMqttClient();
 
 
-            // Ejecutamos la lógica cada vez que llega un mensaje MQTT
-            //_mqttClient.UseApplicationMessageReceivedHandler(equals =>
-            _mqttClient.ApplicationMessageReceivedAsync += async e =>
-            {
-                // 
-                var topic = e.ApplicationMessage.Topic;
-                var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
 
-                // 
-                Console.WriteLine($"Mensaje recibido: {topic} - {payload}");
-                await SaveReading(topic, payload);
-            };
 
-            // Nos conecctamos al broker
-            var options = new MqttClientOptionsBuilder()
-                .WithTcpServer("c89a75e945ca4a3590a418760638ab79.s1.eu.hivemq.cloud", 8883)
-                .WithCredentials("hivemq.webclient.1753150694481", "Q78*&UOb3<McAwqxt$4V")
-                .WithTlsOptions(options =>
+                // Nos suscribimos a los topics cuando el cliente se conecte
+                _mqttClient.ConnectedAsync += async e =>
                 {
-                    options.WithCertificateValidationHandler(context => true);
-                })
-                .Build();
+                    try
+                    {
+                        // 
+                        Console.WriteLine("### COnectando con el broker ###");
+
+                        // Nos suscribimos a los topics de entradas:
+                        await _mqttClient.SubscribeAsync("valor-humedad-1");
+                        await _mqttClient.SubscribeAsync("valor-humedad-2");
+                        await _mqttClient.SubscribeAsync("valor-humedad-3");
+                        await _mqttClient.SubscribeAsync("valor-humedad-4");
+
+                        Console.WriteLine("## Subscrito a los topics ###");
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Error al suscribirse a los topics.");
+                    }
+                };
 
 
-            // 
-            await _mqttClient.ConnectAsync(options);
+
+                // Ejecutamos la lógica cada vez que llega un mensaje MQTT
+                //_mqttClient.UseApplicationMessageReceivedHandler(equals =>
+                _mqttClient.ApplicationMessageReceivedAsync += async e =>
+                {
+                    try
+                    {
+                        // 
+                        var topic = e.ApplicationMessage.Topic;
+                        var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+
+                        // 
+                        Console.WriteLine($"Mensaje recibido: {topic} - {payload}");
+                        await SaveReading(topic, payload);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, $"Error procesando el mensaje: {ex.Message}");
+                    }
+                };
+
+                // Nos conecctamos al broker
+                var options = new MqttClientOptionsBuilder()
+                    .WithTcpServer("c89a75e945ca4a3590a418760638ab79.s1.eu.hivemq.cloud", 8883)
+                    .WithCredentials("hivemq.webclient.1753150694481", "Q78*&UOb3<McAwqxt$4V")
+                    .WithTlsOptions(options =>
+                    {
+                        options.WithCertificateValidationHandler(context => true);
+                    })
+                    .Build();
+
+
+                // 
+                await _mqttClient.ConnectAsync(options);
+            
+            }catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error al iniciar el servicio MQTT: {ex.Message}");
+
+            }
         }
+
+
+
+
+
+
 
         // Interfaz para que "IHostedService" funcione correctamente
         public async Task StopAsync(CancellationToken cancellationToken)
         {
-            if (_mqttClient != null)
+            try
             {
-                await _mqttClient.DisconnectAsync();
+                if (_mqttClient != null)
+                {
+                    await _mqttClient.DisconnectAsync();
+                    Console.WriteLine("### Desconectado del broker ###");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error al detener el servicio MQTT: {ex.Message}");
             }
         }
+
+
+
+
+
+
+
+
 
 
         // Metodo para suscribirme y enviar datos para interactuar con la ESP32
         // Tambien guardamos los datos en la BD.
         private async Task SaveReading(string topic, string payload)
         {
-            using var scope = _scopeFactory.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<CropMonitorDbContext>();
-
-
-            // 
-            int sensorId = topic switch
+            try
             {
-                "valor-humedad-1" => 1,
-                "valor-humedad-2" => 2,
-                "valor-humedad-3" => 3,
-                "valor-humedad-4" => 4,
-                _ => 0
-            };
-
-            // 
-            if (sensorId == 0) return;
-
-            if (!decimal.TryParse(payload, out decimal valor)) return;
-
-            // Guardamos la lectura
-            var lectura = new LecturaSensor
-            {
-                SensorID = sensorId,
-                Timestamp = DateTime.Now,
-                Valor = valor
-            };
+                using var scope = _scopeFactory.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<CropMonitorDbContext>();
 
 
-            // 
-            db.LecturasSensores.Add(lectura);
+                // 
+                int sensorId = topic switch
+                {
+                    "valor-humedad-1" => 1,
+                    "valor-humedad-2" => 2,
+                    "valor-humedad-3" => 3,
+                    "valor-humedad-4" => 4,
+                    _ => 0
+                };
 
-            // Actualizar última lectura en tabla 'Sensores'
-            var sensor = await db.Sensores.FindAsync(sensorId);
-            if (sensor != null)
-            {
-                sensor.UltimaLectura = DateTime.Now;
-                sensor.ValorLectura = valor;
+                // 
+                if (sensorId == 0) return;
+
+                if (!decimal.TryParse(payload, out decimal valor)) return;
+
+                // Guardamos la lectura
+                var lectura = new LecturaSensor
+                {
+                    SensorID = sensorId,
+                    Timestamp = DateTime.Now,
+                    Valor = valor
+                };
+
+
+                // 
+                db.LecturasSensores.Add(lectura);
+
+                // Actualizar última lectura en tabla 'Sensores'
+                var sensor = await db.Sensores.FindAsync(sensorId);
+                if (sensor != null)
+                {
+                    sensor.UltimaLectura = DateTime.Now;
+                    sensor.ValorLectura = valor;
+                }
+
+                await db.SaveChangesAsync();
+                Console.WriteLine($"### Lectura guardada para sensor {sensorId} ###");
             }
-
-            await db.SaveChangesAsync();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error guardando la lectura: {ex.Message}");
+            }
         }
     }
 }
